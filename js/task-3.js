0a4ae5199b717be14e1ba8019e3fa1f22094da03
Dogan Demirbas <dgndmrbas@gmail.com>
2025-05-15 21:42:43 +0300

    fix4

--- js/task-3.js
+++ js/task-3.js
@@ -6,7 +6,7 @@ function getElementWidth(content, padding, border) {
     
     return Number.parseFloat(newContent) + Number.parseFloat(newPadding) * 2 + Number.parseFloat(newBorder) * 2
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+document.write(getElementWidth("50px", "8px", "4px")+ "<br>"); // 74
+document.write(getElementWidth("60px", "12px", "8.5px")+ "<br>"); // 101
+document.write(getElementWidth("200px", "0px", "0px")+ "<br>"); // 200
 
