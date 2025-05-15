36f0cd48973b6c9678aaa202973b4e788ff7f2ea
Dogan Demirbas <dgndmrbas@gmail.com>
2025-05-16 00:22:17 +0300

    fix6

--- js/task-1.js
+++ js/task-1.js
@@ -3,6 +3,6 @@ function makeTransaction(quantity, pricePerDroid) {
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-document.write(makeTransaction(5, 3000)+ "<br>"); // "You ordered 5 droids worth 15000 credits!"
-document.write(makeTransaction(3, 1000)+ "<br>"); // "You ordered 3 droids worth 3000 credits!"
-document.write(makeTransaction(10, 500)+ "<br>"); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000)+ "<br>"); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)+ "<br>"); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)+ "<br>"); // "You ordered 10 droids worth 5000 credits!"
--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
     return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
-document.write(getShippingMessage("Australia", 120, 50)+ "<br>"); // "Shipping to Australia will cost 170 credits"
-document.write(getShippingMessage("Germany", 80, 20)+ "<br>"); // "Shipping to Germany will cost 100 credits"
-document.write(getShippingMessage("Sweden", 100, 20)+ "<br>"); // "Shipping to Sweden will cost 120 credits"
+console.log(getShippingMessage("Australia", 120, 50)+ "<br>"); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)+ "<br>"); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)+ "<br>"); // "Shipping to Sweden will cost 120 credits"
